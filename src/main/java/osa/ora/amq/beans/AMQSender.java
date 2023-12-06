fe4b584546ba7601ebd2ef8b2e1f13631a816f0d
Osama Oransa <osa_ora@yahoo.com>
2023-12-06 16:45:00 +0400

    Update AMQSender.java

--- src/main/java/osa/ora/amq/beans/AMQSender.java
+++ src/main/java/osa/ora/amq/beans/AMQSender.java
@@ -1,13 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package osa.ora.amq.beans;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.core.JmsTemplate;
-
 /**
  *
  * @author ooransa
@@ -19,6 +9,14 @@ public class AMQSender {
 
     public void send(String message,String queueName) {
         System.out.println("Sending message=" + message);
+        jmsTemplate.setPubSubDomain(false);
         jmsTemplate.convertAndSend(queueName, message);
     }
+
+    public void publish(String message,String queueName) {
+        System.out.println("Publish topic message=" + message);
+        jmsTemplate.setPubSubDomain(true);
+        jmsTemplate.convertAndSend(queueName, message);
+    }
+
 }
