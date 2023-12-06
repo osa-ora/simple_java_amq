ac0cc947cd18f51440e243e4d221554b30b89525
Osama Oransa <osa_ora@yahoo.com>
2023-12-06 16:43:12 +0400

    Update AmqApplication.java

--- src/main/java/osa/ora/amq/AmqApplication.java
+++ src/main/java/osa/ora/amq/AmqApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AmqApplication {
     public static final String QUEUE_NAME="my-queue";
+    //public static final String QUEUE_NAME="my-topic";
 
     public static void main(String[] args) {
         SpringApplication.run(AmqApplication.class, args);
