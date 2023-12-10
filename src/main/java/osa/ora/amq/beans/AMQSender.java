4a07d0655761df02051e00f5ee9ac228ebb7633c
Osama Oransa <osa_ora@yahoo.com>
2023-12-10 11:46:45 +0200

    Update AMQSender.java

--- src/main/java/osa/ora/amq/beans/AMQSender.java
+++ src/main/java/osa/ora/amq/beans/AMQSender.java
@@ -1,3 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package osa.ora.amq.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 /**
  *
  * @author ooransa
